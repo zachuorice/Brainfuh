1539306b8d058c59b22f470d91c142442c7d956b
Zachary Richey <zr.public@gmail.com>
2012-12-10 15:22:15 -0500

    Ant build.xml skeleton

---
+++ src/com/github/zachuorice/brainfuh/Brainfuh.java
@@ -0,0 +1,8 @@
+/* Useful static methods for using the library quickly. */
+package com.github.zachuorice.brainfuh;
+import com.github.zachuorice.brainfuh.*;
+
+public class Brainfuh
+{
+    // TODO
+}
