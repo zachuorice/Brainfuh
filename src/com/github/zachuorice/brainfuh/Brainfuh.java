a157a22253509856068045f8318a4c64b38daeb9
Zachary Richey <zr.public@gmail.com>
2012-12-12 22:09:17 -0500

    Brainfuh static methods done

--- src/com/github/zachuorice/brainfuh/Brainfuh.java
+++ src/com/github/zachuorice/brainfuh/Brainfuh.java
@@ -1,16 +1,29 @@
 /* Useful static methods for using the library quickly. */
 package com.github.zachuorice.brainfuh;
-import com.github.zachuorice.brainfuh.*;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import com.github.zachuorice.brainfuh.*;
 
 public class Brainfuh
 {
     static void executeFile(File code) throws IOException
     {
         if(!code.canRead() || !code.isFile())
-        {
             throw new IOException();
-        }
+        FileReader code_reader = new FileReader(code);
+        int data = code_reader.read();
+        Interpreter interpreter = new Interpreter();
+        while(data != -1)
+            interpreter.feed(data);
+        interpreter.execute();
+    }
+
+    static void executeString(String code)
+    {
+        Interpreter interpreter = new Interpreter();
+        for(int index=0; index < code.length(); index++)
+            interpreter.feed(code.charAt(index));
+        interpreter.execute();
     }
 }
--- src/com/github/zachuorice/brainfuh/Interpreter.java
+++ src/com/github/zachuorice/brainfuh/Interpreter.java
@@ -3,4 +3,13 @@ package com.github.zachuorice.brainfuh;
 
 public class Interpreter
 {
+    void feed(int data) 
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void execute()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
