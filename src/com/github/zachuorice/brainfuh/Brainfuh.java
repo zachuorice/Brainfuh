6d637da3de9c20e28575f2788653f6dbcd2c9bb4
Zachary Richey <zr.public@gmail.com>
2012-12-13 13:29:00 -0500

    Interpreter done except for step().

--- src/com/github/zachuorice/brainfuh/Brainfuh.java
+++ src/com/github/zachuorice/brainfuh/Brainfuh.java
@@ -1,3 +1,20 @@
+/* 
+ *  Copyright 2012 Zachary Richey <zr.public@gmail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 /* Useful static methods for using the library quickly. */
 package com.github.zachuorice.brainfuh;
 import java.io.File;
@@ -15,15 +32,14 @@ public class Brainfuh
         int data = code_reader.read();
         Interpreter interpreter = new Interpreter();
         while(data != -1)
-            interpreter.feed(data);
+            interpreter.feed((char ) data);
         interpreter.execute();
     }
 
     static void executeString(String code)
     {
         Interpreter interpreter = new Interpreter();
-        for(int index=0; index < code.length(); index++)
-            interpreter.feed(code.charAt(index));
+        interpreter.feed(code);
         interpreter.execute();
     }
 }
--- src/com/github/zachuorice/brainfuh/Interpreter.java
+++ src/com/github/zachuorice/brainfuh/Interpreter.java
@@ -1,5 +1,25 @@
+/* 
+ *  Copyright 2012 Zachary Richey <zr.public@gmail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 /* A brainf*** interpreter in Java */
 package com.github.zachuorice.brainfuh;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /* 
  * This object represents a single instruction 
@@ -7,7 +27,7 @@ package com.github.zachuorice.brainfuh;
  */
 class Instruction
 {
-    public enum SymbolType
+    public enum Type
     {
         IGNORED,
         INC_DP('>'), 
@@ -19,18 +39,23 @@ class Instruction
         ZERO_JMP('['),
         NZ_JMP(']');
 
-        SymbolType() {this.symbol = -1;}
-        SymbolType(char symbol) {this.symbol = symbol;}
+        Type() {this.symbol = -1;}
+        Type(char symbol) {this.symbol = symbol;}
 
         private int symbol;
         public int symbol() {return this.symbol;}
 
-        static SymbolType match(char symbol)
+        static Type match(char symbol)
         {
-            for(SymbolType sym : SymbolType.values())
+            for(Type sym : Type.values())
                 if(sym.symbol() == symbol)
                     return sym;
             return IGNORED;
+        }
+
+        static boolean ignored(char symbol)
+        {
+            return Type.match(symbol) == Type.IGNORED;
         }
     }
 
@@ -40,32 +65,139 @@ class Instruction
     private int col_no;
     public int col() {return this.col_no;}
 
-    private SymbolType type;
-    public SymbolType type() {return this.type;}
+    private Type type;
+    public Type type() {return this.type;}
 
     Instruction(int line, int col, char instruction)
     {
         this.line_no = line;
         this.col_no = col;
-        this.type = SymbolType.match(instruction);
+        this.type = Type.match(instruction);
     }
 }
 
-public class Interpreter
+/**
+ * An interpreter for Brainf*** commands. 
+ */
+public final class Interpreter
 {
+    /**
+     * The size of the data segment, and thus how much memory
+     * a brainf*** program can address.
+     */
+    public static final int DATA_SIZE = 30000;
 
+    // Instruction segment variables
+    private LinkedList<Instruction> instructions;
+    private ListIterator<Instruction> instruction_pointer;
+
+    // Data segment variables
+    private byte[] data;
+    private int data_pointer;
+
+    // Line and column number tracking variables, note
+    // that CURRENT line and column number information
+    // can be found in the instruction pointer not here.
+    private int line_no;
+    private int col_no;
+
+    /**
+     * Reset the data segment and instruction pointer.
+     */
+    void reset()
+    {
+        this.data = new byte[DATA_SIZE];
+        this.data_pointer = 0;
+        this.instruction_pointer = (ListIterator<Instruction>) 
+                                    this.instructions.iterator();
+    }
+
+    /**
+     * Discard all fed instructions.
+     */
+    void clear()
+    {
+        this.instructions = new LinkedList<>();
+        this.line_no = 1;
+        this.col_no = 1;
+    }
+
+    int line()
+    {
+        int line;
+        try
+        {
+            line = instruction_pointer.next().line();
+            instruction_pointer.previous();
+        }
+        catch(NoSuchElementException e)
+        {
+            line = -1;
+        }
+        return line;
+    }
+
+    int col()
+    {
+        int col;
+        try
+        {
+            col = instruction_pointer.next().col();
+            instruction_pointer.previous();
+        }
+        catch(NoSuchElementException e)
+        {
+            col = -1;
+        }
+        return col;
+    }
+
+    /** 
+     * Feed a single instruction to the Interpreter, which will be stored
+     * in the instruction segment and executed when execute() is called.
+     */
     void feed(char data) 
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        // Increment line or column number information
+        if(data == '\n' || data == '\r')
+        {
+            // This conditional handles CLRF newlines.
+            if(this.col_no != 0)
+                this.line_no += 1;
+            this.col_no = 0;
+        }
+        else
+            this.col_no += 1;
+
+        if(!Instruction.Type.ignored(data))
+            instructions.add(new Instruction(this.line_no, this.col_no, data));
     }
 
     void feed(String data)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        for(int index=0; index < data.length(); index++)
+            feed(data.charAt(index));
     }
 
-    void execute()
+    /**
+     * reset() the interpreter and execute the fed instructions from beginning
+     * to end.
+     */
+    void execute() throws DataOverflowException, DataUnderflowException
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        reset();
+        while(instruction_pointer.hasNext())
+            step();
+    }
+
+    void step() throws ProgramDoneException
+    {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    Interpreter()
+    {
+        clear();
+        reset();
     }
 }
